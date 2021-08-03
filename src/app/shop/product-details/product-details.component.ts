1d7ea3d4251e074236009c9507f28b6b0b38a3fc
Raphael Okeibunor <raphaelnagato19@gmail.com>
2021-08-03 10:02:00 +0100

    setup section-header title

--- src/app/shop/product-details/product-details.component.ts
+++ src/app/shop/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IProduct } from 'src/app/shared/models/product';
+import { BreadcrumbService } from 'xng-breadcrumb';
 import { ShopService } from '../shop.service';
 
 @Component({
@@ -10,15 +11,24 @@ import { ShopService } from '../shop.service';
 })
 export class ProductDetailsComponent implements OnInit {
   product!: IProduct;
-  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute) {}
+  constructor(
+    private shopService: ShopService,
+    private activatedRoute: ActivatedRoute,
+    private bcService: BreadcrumbService
+  ) {}
 
   ngOnInit(): void {
     this.loadProduct();
   }
   loadProduct() {
-    this.shopService.getProduct(Number(this.activatedRoute.snapshot.paramMap.get('id'))).subscribe(
-      (response) => (this.product = response),
-      (err) => console.log(err)
-    );
+    this.shopService
+      .getProduct(Number(this.activatedRoute.snapshot.paramMap.get('id')))
+      .subscribe(
+        (response) => {
+          this.product = response;
+          this.bcService.set('@productDetails', this.product.name);
+        },
+        (err) => console.log(err)
+      );
   }
 }
