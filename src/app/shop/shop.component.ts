d6403055530750077362c23612e2602565d949b1
Raphael Okeibunor <raphaelnagato19@gmail.com>
2021-09-16 17:52:49 +0100

    Make sure components load at same time

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -11,7 +11,7 @@ import { ShopService } from './shop.service';
   styleUrls: ['./shop.component.scss'],
 })
 export class ShopComponent implements OnInit {
-  @ViewChild('search', { static: true }) SearchTerm!: ElementRef;
+  @ViewChild('search', { static: false }) SearchTerm!: ElementRef;
   products: IProduct[] = [];
   brands!: IBrand[];
   productTypes!: IProductType[];
